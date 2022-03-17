53c73da90f7183b4b9ea6df5e09d92b461dabcf8
dawood19582 <63267781+dawood19582@users.noreply.github.com>
2022-03-17 00:30:12 -0400

    correcting the code
    when Itried the code by using the input string "12-34-56-78-9A-BG" it gives me True and this is should be false,
    I made sum correction in the second if statment to give the correct result
    best regards
    and good job

--- Arcade/BookMarket/IsMAC48Address.py
+++ Arcade/BookMarket/IsMAC48Address.py
@@ -32,7 +32,8 @@ def isMAC48Address(inputString):
     for i in range(0,len(inputStringSplit)):
         if len(inputStringSplit[i])!=2:
             return False
-        if (("0"<=inputStringSplit[i][0]<="9" or "A"<=inputStringSplit[i][0]<="F") and ("0"<=inputStringSplit[i][1]<="9" or "A"<=inputStringSplit[i][1]<="F")):
-            return True
-        else:
+         if not (("0"<=inputStringSplit[i][0]<="9" or "A"<=inputStringSplit[i][0]<="F") and ("0"<=inputStringSplit[i][1]<="9" or "A"<=inputStringSplit[i][1]<="F")):
             return False
+            break
+    return True        
+    
